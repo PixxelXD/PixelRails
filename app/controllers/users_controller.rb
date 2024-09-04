05e029de86f7b6d24d35c5454708c92477a33e79
pixel <pixel@odds.team>
2024-09-04 21:07:46 +0700

    feat: Add create user function for trying to add user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,11 +6,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: 'User was successfully created.'
+    else
+      render :index
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :birth_date, :gender, :email, :phone_number, :subject)
   end
 end
