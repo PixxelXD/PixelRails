b9661a7f3e1fcfa0a1f1a717fc56e2524bb2b96e
pixel <pixel@odds.team>
2024-09-04 19:22:10 +0700

    feat: Add validation for required fields in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  validates :first_name, :last_name, :birth_date, :gender, :email, :phone_number, :subject, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true
 end
